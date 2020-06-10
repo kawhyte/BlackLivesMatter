f2cbf71518ed9e19f7cb6263c48330ffe04b7455
Kenny <kawhyte@gmail.com>
2020-06-10 15:22:43 -0700

    Label background

--- src/Labels.js
+++ src/Labels.js
@@ -19,19 +19,19 @@ function Labels(props) {
 
   return (
     <div>
-      <div className="flex  mt-10">
-        <span className="text-gray-600 text-xl mt-10 mr-2 ">
+      <div className="flex md:mt-6   mt-0  mb-16 sm:mt-0 ">
+        {/* <span className="text-gray-600 text-xl mt-10 mr-2 ">
           {" "}
           Filter by Tags:
-        </span>
-        <ul className="flex flex-wrap justify-center max-w-6xl">
+        </span> */}
+        <ul className="flex flex-wrap justify-center max-w-6xl bg-gray-200 p-6 ml-6 mr-6">
           {arrayWithoutDuplicates.map((item, i) => {
             if (item !== null) {
               return (
                 <li key={i}>
                   <button
                     onClick={e => onClicked({ item })}
-                    className="mb-20 cursor-pointer m-1 text-xs font-semibold inline-block py-1 px-2 uppercase rounded border bg-gray-100  hover:bg-indigo-500 hover:text-white text-gray-700 uppercase"
+                    className=" cursor-pointer m-1 text-xs font-semibold inline-block py-1 px-2 uppercase rounded border bg-gray-100  hover:bg-indigo-500 hover:text-white text-gray-700 uppercase"
                   >
                     {item}
                   </button>
