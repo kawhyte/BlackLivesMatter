1ed3fa14191602ba8b51afee03bea1059ed50766
Kenny <kawhyte@gmail.com>
2020-06-19 14:03:20 -0700

    Cat

--- src/Categories.js
+++ src/Categories.js
@@ -40,18 +40,18 @@ function Categories() {
           <h2 class="text-md font-medium text-gray-700">Podcasts</h2>
           </Link>
         </div>
-        <div class="bg-gray-200 px-2 py-2 m-2 rounded-lg text-center w-56 transition duration-500 ease-in-out bg-gray-200 hover:bg-gray-300 transform hover:-translate-y-1 hover:scale-110 ">
+        {/* <div class="bg-gray-200 px-2 py-2 m-2 rounded-lg text-center w-56 transition duration-500 ease-in-out bg-gray-200 hover:bg-gray-300 transform hover:-translate-y-1 hover:scale-110 ">
           <div class="mb-3">
             <img class="w-auto mx-auto h-12  " src={article} alt="" />
           </div>
           <h2 class="text-md font-medium text-gray-700">Articles</h2>
-        </div>
-        <div class="bg-gray-200 px-2 py-2 m-2 rounded-lg text-center w-56 transition duration-500 ease-in-out bg-gray-200 hover:bg-gray-300 transform hover:-translate-y-1 hover:scale-110 ">
+        </div> */}
+        {/* <div class="bg-gray-200 px-2 py-2 m-2 rounded-lg text-center w-56 transition duration-500 ease-in-out bg-gray-200 hover:bg-gray-300 transform hover:-translate-y-1 hover:scale-110 ">
           <div class="mb-3">
             <img class="w-auto mx-auto h-12  " src={business} alt="" />
           </div>
           <h2 class="text-md font-medium text-gray-700">Businesses</h2>
-        </div>
+        </div> */}
       </div>
     </div>
   )
