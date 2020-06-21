a2a348fe1ec5176abb8657e3bed3d4419b00c613
Kenny <kawhyte@gmail.com>
2020-06-21 10:28:36 -0700

    categories

--- src/Categories.js
+++ src/Categories.js
@@ -7,39 +7,40 @@ import business from "./img/small-business.png"
 import { Link } from "gatsby"
 function Categories() {
   return (
-    <div className="justify-center mx-auto px-6 pb-6 sm:px-12  sm:flex-row items-center ">
+    <div className="justify-center mx-auto px-6 pb-4 sm:px-12  sm:flex-row items-center ">
       <div className="mt-20 text-xl flex justify-center font-bold py-2 sm:px-12 items-center">
         Categories
       </div>
 
-      <div className="flex flex-wrap justify-center ">
+      <div className="flex flex-wrap justify-center">
         <Link to="/books">
-          <div class="bg-gray-200 px-2 py-2 m-2 rounded-lg text-center w-56 transition duration-500 ease-in-out bg-gray-200 hover:bg-gray-300 transform hover:-translate-y-1 hover:scale-110">
+          <div class="flex justify-center bg-gray-200 px-2 py-2 m-2 rounded-lg text-center w-40 transition duration-500 ease-in-out bg-gray-200 hover:bg-gray-300 transform hover:-translate-y-1 hover:scale-110">
             <div class="mb-3">
-              <img class="w-auto mx-auto h-12 " src={book} alt="" />
+              <img class="w-auto mx-auto h-12 " src={book} alt="books" />
             </div>
-            <h2 class="text-md font-medium text-gray-700">Books</h2>
+            <h2 class="ml-4 pt-4 text-md font-medium text-gray-700">Books</h2>
           </div>
         </Link>
-        <div class="bg-gray-200 px-2 py-2 m-2 rounded-lg text-center w-56 transition duration-500 ease-in-out bg-gray-200 hover:bg-gray-300 transform hover:-translate-y-1 hover:scale-110">
-        <Link to="/movies">
-          <div class="mb-3">
-            <img class="w-auto mx-auto h-12  " src={movie} alt="" />
-          </div>
-          <h2 class="text-md font-medium text-gray-700">
-            Movies/Documentaries
-          </h2>
-          </Link>
-        </div>
 
-        <div class="bg-gray-200 px-2 py-2 m-2 rounded-lg text-center w-56 transition duration-500 ease-in-out bg-gray-200 hover:bg-gray-300 transform hover:-translate-y-1 hover:scale-110 ">
-        <Link to="/podcasts">
+        <Link to="/movies">
+        <div class="flex justify-center bg-gray-200 px-2 py-2 m-2 rounded-lg text-center w-40 transition duration-500 ease-in-out bg-gray-200 hover:bg-gray-300 transform hover:-translate-y-1 hover:scale-110">
           <div class="mb-3">
-            <img class="w-auto mx-auto h-12  " src={podcast} alt="" />
+            <img class="w-auto mx-auto h-12  " src={movie} alt="movies" />
           </div>
-          <h2 class="text-md font-medium text-gray-700">Podcasts</h2>
-          </Link>
+          <h2 class="ml-4 pt-4 text-md font-medium text-gray-700">
+            Movies
+          </h2>
         </div>
+        </Link>
+
+        <Link to="/podcasts">
+        <div class="flex justify-center bg-gray-200 px-2 py-2 m-2 rounded-lg text-center w-40 transition duration-500 ease-in-out bg-gray-200 hover:bg-gray-300 transform hover:-translate-y-1 hover:scale-110 ">
+          <div class="mb-3">
+            <img class="w-auto mx-auto h-12  " src={podcast} alt="podcast" />
+          </div>
+          <h2 class="ml-4 pt-4 text-md font-medium text-gray-700">Podcasts</h2>
+        </div>
+          </Link>
         {/* <div class="bg-gray-200 px-2 py-2 m-2 rounded-lg text-center w-56 transition duration-500 ease-in-out bg-gray-200 hover:bg-gray-300 transform hover:-translate-y-1 hover:scale-110 ">
           <div class="mb-3">
             <img class="w-auto mx-auto h-12  " src={article} alt="" />
