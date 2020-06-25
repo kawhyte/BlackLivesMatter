8bcab3a24353ceedb5728c7c5ae5c0a371cc2852
Kenny <kawhyte@gmail.com>
2020-06-25 15:45:49 -0700

    business card

--- src/BusinessCardList.js
+++ src/BusinessCardList.js
@@ -99,50 +99,52 @@ const BusinessCardList = () => {
         <Labels labels={labels} onClicked={handleButtonClicked} activeButton= {activeButton} />
       </div>
       {FoodBusinessToRender.length > 0 ? (
-         <div className="mt-10  flex container flex-wrap justify-between text-xl mb-0 mx-auto px-6 sm:px-12 flex flex-col-reverse sm:flex-row items-end bg-gray-100">
+         <div className="mt-10 flex container flex-wrap justify-between text-xl mb-0 mx-auto px-6 sm:px-12 flex flex-col-reverse sm:flex-row ">
          <p className="text-md font-bold p-2 ">Food</p>
        </div>
       ) : (
         ""
       )}
+ 
+      <div className="gap-4 mx-auto container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  ">
+    
+         {FoodBusinessToRender}
 
-      <div className="gap-4 mx-auto container grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
-        {FoodBusinessToRender}
       </div>
 
       {EtsyBusinessToRender.length > 0 ? (
-         <div className="mt-10  flex container flex-wrap justify-between text-xl mb-0 mx-auto px-6 sm:px-12 flex flex-col-reverse sm:flex-row items-end bg-gray-100">
+            <div className="mt-10 flex container flex-wrap justify-between text-xl mb-0 mx-auto px-6 sm:px-12 flex flex-col-reverse sm:flex-row">
          <p className="text-md font-bold p-2 ">Etsy Sellers</p>
        </div>
       ) : (
         ""
       )}
 
-      <div className="gap-4 mx-auto container grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+<div className="gap-4 mx-auto container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {EtsyBusinessToRender}
       </div>
 
       {CosmeticsBusinessToRender.length > 0 ? (
-         <div className="mt-10  flex container flex-wrap justify-between text-xl mb-0 mx-auto px-6 sm:px-12 flex flex-col-reverse sm:flex-row items-end bg-gray-100">
+         <div className="mt-10 flex container flex-wrap justify-between text-xl mb-0 mx-auto px-6 sm:px-12 flex flex-col-reverse sm:flex-row">
          <p className="text-md font-bold p-2 ">Cosmetics</p>
        </div>
       ) : (
         ""
       )}
 
-      <div className="gap-4 mx-auto container grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+<div className="gap-4 mx-auto container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  ">
         {CosmeticsBusinessToRender}
       </div>
 
       {HomeBusinessToRender.length > 0 ? (
-         <div className="mt-10  flex container flex-wrap justify-between text-xl mb-0 mx-auto px-6 sm:px-12 flex flex-col-reverse sm:flex-row items-end bg-gray-100">
+         <div className="mt-10 flex container flex-wrap justify-between text-xl mb-0 mx-auto px-6 sm:px-12 flex flex-col-reverse sm:flex-row">
          <p className="text-md font-bold p-2 ">Home & Living</p>
        </div>
       ) : (
         ""
       )}
 
-      <div className="gap-4 mx-auto container grid grid-cols-2 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+<div className="gap-4 mx-auto container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {HomeBusinessToRender}
       </div>
     </>
