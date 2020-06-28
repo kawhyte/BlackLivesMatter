dc103cd3daed7bd1d44cbed2e9a38d13ad2641d6
Kenny <kawhyte@gmail.com>
2020-06-27 19:44:10 -0700

    image update

--- src/Labels.js
+++ src/Labels.js
@@ -25,7 +25,8 @@ function Labels(props) {
     
 
     style={{
-  backgroundImage: `url('https://res.cloudinary.com/babyhulk/image/upload/f_auto/v1593291021/Race/bg/andrew-ridley-jR4Zf-riEjI-unsplash.jpg')`,
+  // backgroundImage: `url('https://res.cloudinary.com/babyhulk/image/upload/f_auto/v1593291021/Race/bg/andrew-ridley-jR4Zf-riEjI-unsplash.jpg')`,
+  backgroundImage: `url('https://res.cloudinary.com/babyhulk/image/upload/f_auto/v1593312159/Race/bg/bg_2.jpg')`,
   backgroundRepeat: 'no-repeat',
   backgroundSize: 'cover',
   backgroundPosition: 'center',
