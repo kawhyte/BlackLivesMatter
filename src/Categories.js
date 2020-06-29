3f65bd359b05e3f41b14f0ce8a364eb9487f0ecb
Kenny <kawhyte@gmail.com>
2020-06-28 23:11:46 -0700

    Categories

--- src/Categories.js
+++ src/Categories.js
@@ -8,26 +8,26 @@ import { Link } from "gatsby"
 function Categories() {
   return (
     <div className="sm:hidden inline-block justify-center mx-auto px-6 pb-1 sm:px-12  sm:flex-row items-center ">
-      <div className="mt-8 text-xl flex justify-center font-bold py-2 sm:px-12 items-center">
+      <div className="mt-4 text-xl flex justify-center font-bold py-2 sm:px-12 items-center">
         Categories
       </div>
 
       <div className="flex flex-wrap justify-center">
         <Link to="/books">
           <div className="flex justify-center  px-2 py-2 m-2 rounded-lg text-center w-40 transition duration-500 ease-in-out bg-gray-200 hover:bg-gray-300 transform hover:-translate-y-1 hover:scale-110">
-            <div className="mb-3">
-              <img className="w-auto mx-auto h-12 " src={book} alt="books" />
+            <div className="mb-1">
+              <img className="w-auto mx-auto h-8 " src={book} alt="books" />
             </div>
-            <h2 className="ml-4 pt-4 text-md font-medium text-gray-700">Books</h2>
+            <h2 className="ml-4 pt-2 text-md font-medium text-gray-700">Books</h2>
           </div>
         </Link>
 
         <Link to="/movies">
         <div className="flex justify-center px-2 py-2 m-2 rounded-lg text-center w-40 transition duration-500 ease-in-out bg-gray-200 hover:bg-gray-300 transform hover:-translate-y-1 hover:scale-110">
-          <div className="mb-3">
-            <img className="w-auto mx-auto h-12  " src={movie} alt="movies" />
+          <div className="mb-1">
+            <img className="w-auto mx-auto h-8  " src={movie} alt="movies" />
           </div>
-          <h2 className="ml-4 pt-4 text-md font-medium text-gray-700">
+          <h2 className="ml-4 pt-2 text-md font-medium text-gray-700">
             Movies
           </h2>
         </div>
@@ -35,19 +35,19 @@ function Categories() {
 
         <Link to="/podcasts">
         <div className="flex justify-center  px-2 py-2 m-2 rounded-lg text-center w-40 transition duration-500 ease-in-out bg-gray-200 hover:bg-gray-300 transform hover:-translate-y-1 hover:scale-110 ">
-          <div className="mb-3">
-            <img className="w-auto mx-auto h-12  " src={podcast} alt="podcast" />
+          <div className="mb-1">
+            <img className="w-auto mx-auto h-8  " src={podcast} alt="podcast" />
           </div>
-          <h2 className="ml-4 pt-4 text-md font-medium text-gray-700">Podcasts</h2>
+          <h2 className="ml-4 pt-2 text-md font-medium text-gray-700">Podcasts</h2>
         </div>
           </Link>
     
         <Link to="/business">
         <div className="flex justify-center px-2 py-2 m-2 rounded-lg text-center w-40 transition duration-500 ease-in-out bg-gray-200 hover:bg-gray-300 transform hover:-translate-y-1 hover:scale-110 ">
-          <div className="mb-3">
-            <img className="w-auto mx-auto h-12  " src={business} alt="business icon" />
+          <div className="mb-1">
+            <img className="w-auto mx-auto h-8  " src={business} alt="business icon" />
           </div>
-          <h2 className="ml-4 pt-4 text-md font-medium text-gray-700">Businesses</h2>
+          <h2 className="ml-4 pt-2 text-md font-medium text-gray-700">Businesses</h2>
         </div>
         </Link>
       </div>
