ba7e3ee1a93c0af98e90254cc116c446091356da
Kenny <kawhyte@gmail.com>
2020-06-29 11:26:10 -0700

    test2

--- src/HomePageComponents/FeaturedItems.js
+++ src/HomePageComponents/FeaturedItems.js
@@ -4,13 +4,13 @@ import { paginate } from "../common/paginate"
 import { createCards } from "../common/createCards"
 import CreatePageSection from "../HomePageComponents/CreatePageSection"
 
-
 function FeaturedItems() {
   const data = useStaticQuery(graphql`
     {
-      allAirtable(filter: {table: {eq: "Books"}}) {
+      allAirtable(filter: { table: { eq: "Books" } }) {
         nodes {
           id
+          recordId
           data {
             Slug
             Featured
@@ -46,31 +46,23 @@ function FeaturedItems() {
     pageSize: 100,
   })
 
-  
   const newPages = paginate(state, pageState.currentPage, pageState.pageSize)
   let books = createCards(newPages)
 
   const itemsToRender = books.filter(item => {
-    
     return (
-      (item.props.category.includes("Book") && item.props.featured === true)||
+      (item.props.category.includes("Book") && item.props.featured === true) ||
       (item.props.category.includes("Movie") && item.props.featured === true) ||
       (item.props.category.includes("Podcast") && item.props.featured === true)
-      
-      )
+    )
   })
-
 
   return (
     <>
-    {/* <div className="flex justify-center flex-col  py-8"> */}
-   
-    <CreatePageSection business={itemsToRender} renderDetails ={true}/>
-
-     {/* </div> */}
-
-     </>
-
+      {/* <div className="pl-4  gap-4 mx-auto container grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 bg-red-100 "> */}
+        <CreatePageSection business={itemsToRender} renderDetails={true} />
+      {/* </div> */}
+    </>
   )
 }
 
--- src/HomePageComponents/createPageSection.js
+++ src/HomePageComponents/createPageSection.js
@@ -5,6 +5,8 @@ let bgColor = ""
 let categoriesToRender = []
 
 function CreatePageSection({ business, renderDetails }) {
+
+  console.log("@@@@business ", business)
   //get categories
   business.map((label, i) => {
     if (!categoriesToRender.includes(label.props.category)) {
@@ -13,65 +15,106 @@ function CreatePageSection({ business, renderDetails }) {
     return categoriesToRender
   })
 
-   console.log("categoriesToRender ",categoriesToRender)
-   console.log("business ",business)
-  return (
-    <>
-      {categoriesToRender.map((item, i) => {
+  console.log("categoriesToRender ", categoriesToRender)
+  
 
-        if (renderDetails) {
-          if (i % 2 === 0) {
-            bgColor = "bg-blue-100"
-          } else {
-            bgColor = "bg-gray-100"
-          }
-        } else {
-          bgColor = "bg-white"
-        }
+  let newCat = categoriesToRender.map((item, i) => {
+    if (renderDetails) {
+      if (i % 2 === 0) {
+        bgColor = "bg-blue-100"
+      } else {
+        bgColor = "bg-gray-100"
+      }
+    } else {
+      bgColor = "bg-white"
+    }
 
-        let links = item.toLowerCase()
+    let links = item.toLowerCase()
 
-        itemToRender = business.filter(card => {
-          return card.props.category.includes(item)
-        })
+    itemToRender = business.filter(card => {
+      return card.props.category.includes(item)
+    })
 
-        if (item !== null) {
-          return (
-            <div>
-              {/* {itemToRender.length > 0 && renderDetails ? (
-                <div key={business.id}
-                  className={
-                    "mt-10  flex container flex-wrap justify-between text-2xl  mx-auto px-6 sm:px-12  flex-col-reverse sm:flex-row " +
-                    bgColor
-                  }
+    console.log("item- ", item)
+    console.log("itemToRender$$$ ", itemToRender)
+
+    if (item !== null) {
+      if (itemToRender.length > 0 && renderDetails) {
+        return (
+          <div key= {itemToRender.recordId}>
+            <div
+              
+              className={
+                "mt-10  flex container flex-wrap justify-between text-2xl  mx-auto px-6 sm:px-12  flex-col-reverse sm:flex-row " +
+                bgColor
+              }
+            >
+              <p className={"font-bold pb-8 pt-4"}>Featured {item}s</p>
+              <p className="text-sm pt-4">
+                <Link
+                  key={business.id}
+                  className="text-blue-500"
+                  to={`/${links}s`}
                 >
-                  
-                  <p  className={"font-bold pb-8 pt-4"}>Featured {item}s</p>
-                  <p  className="text-sm pt-4">
-                    <Link key={business.id} className="text-blue-500" to={`/${links}s`}>
-                      Explore All {item}s
-                    </Link>
-                  </p>
-                </div>
-              ) : "text"
-              } */}
-              <div 
-                className={ 
-                  "pl-4  gap-4 mx-auto container grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4  " +
-                  bgColor
-                } 
-              >
-                {itemToRender}
-              </div>
+                  Explore All {item}s
+                </Link>
+              </p>
             </div>
-          )
-        }
-        //return " "
-      })}
-    </>
-  )
+            <div key= {itemToRender.recordId}
+              className={
+                "pl-4  gap-4 mx-auto container grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4  " +
+                bgColor
+              }
+            >
+              {itemToRender}
+            </div>
+          </div>
+        )
+      } else {
+        return (
+          <div key= {itemToRender.recordId}
+            // className={
+            //   "pl-4  gap-4 mx-auto container grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4  " +
+            //   bgColor
+            // }
+          >
+            {itemToRender}
+          </div>
+        )
+      }
+
+      // return (
+      //    <div>
+      //      {(itemToRender.length > 0 && renderDetails) ? (
+      //       <div key={business.id}
+      //         className={
+      //           "mt-10  flex container flex-wrap justify-between text-2xl  mx-auto px-6 sm:px-12  flex-col-reverse sm:flex-row " +
+      //           bgColor
+      //         }
+      //       >
+      //         <p  className={"font-bold pb-8 pt-4"}>Featured {item}s</p>
+      //         <p  className="text-sm pt-4">
+      //           <Link key={business.id} className="text-blue-500" to={`/${links}s`}>
+      //             Explore All {item}s
+      //           </Link>
+      //         </p>
+      //       </div>
+      //     ) : "text"
+      //     }
+      //     <div
+      //       className={
+      //         "pl-4  gap-4 mx-auto container grid grid-cols-2 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4  " +
+      //         bgColor
+      //       }
+      //     >
+      //       {itemToRender}
+      //     </div>
+      //    </div>
+      // )
+    }
+  })
+
+  return <>{newCat}</>
 }
 
 export default CreatePageSection
-
-
