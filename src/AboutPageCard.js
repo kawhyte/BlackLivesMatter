4efc5606aa0b896df03a6fe0d9dd23b6e2127d7b
Kenny <kawhyte@gmail.com>
2020-07-02 12:39:09 -0700

    added image

--- src/AboutPageCard.js
+++ src/AboutPageCard.js
@@ -1,19 +1,52 @@
 import React from "react"
 import { Link } from "gatsby"
+import noImage from "./img/noPhotoFound.jpg"
 
 function AboutPageCard({ name, born, death, image, link }) {
+
+  console.log("@@@@", image)
+
+  // image === null ? noImage : image
   return (
-    <div class="bg-white overflow-hidden ">
-      <div className="text-center p-6 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
-        <a href={link} rel="noreferrer" target="_blank" className="">
-          {/* <img class="rounded-full mx-auto  " src={image} alt={name} /> */}
-          <p class="pt-2 text-lg font-semibold text-blue-500 ">{name}</p>
-          <p class="text-sm text-gray-600">
-            {born} - {death}
-          </p>
-        </a>
-      </div>
-    </div>
+    // <div class="bg-white overflow-hidden ">
+    //   <div className="text-center p-6 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110">
+    //     <a href={link} rel="noreferrer" target="_blank" className="">
+    //       {/* <img class="rounded-full mx-auto  " src={image} alt={name} /> */}
+    //       <p class="pt-2 text-lg font-semibold text-blue-500 ">{name}</p>
+    //       <p class="text-sm text-gray-600">
+    //         {born} - {death}
+    //       </p>
+    //     </a>
+    //   </div>
+    // </div>
+
+
+    <div
+    class="mb-4 md:mb-0 w-full max-w-screen-md mx-auto relative h-auto transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110"
+    style={{ height: '15em'  }} 
+  >
+    {/* <div
+      class="absolute left-0 bottom-0 w-full h-full z-10"
+      style={{
+        backgroundImage:
+          "linear-gradient`(180deg,transparent,rgba(0,0,0,.7))",
+      }}
+    ></div> */}
+    <a href={link} rel="noreferrer" target="_blank" className="">
+    <img
+      src={image === null ? noImage : image}//"https://images.unsplash.com/photo-1590989995665-1815e7b2c42b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1526&q=80"
+      className="absolute left-0 top-0 w-full h-full z-0 object-cover"
+      alt={name}
+    />
+    <div class="p-4 absolute bottom-0 inset-x-0 z-20">
+      <h2 class="text-lg font-semibold text-gray-400 leading-tight">
+     {name}
+           <p class="text-xs text-gray-100">
+             {born} - {death}
+           </p>
+      </h2>
+    </div></a>
+  </div>
 
   )
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,81 +1,128 @@
-import React from 'react'
+import React from "react"
 import NavBar from "../NavBar"
 import Footer from "../Footer"
-import AboutPageCard from '../AboutPageCard'
-import AboutPageList from '../AboutPageList'
+import AboutPageCard from "../AboutPageCard"
+import AboutPageList from "../AboutPageList"
 
 function about() {
-  
-    return (
-<div>
+  return (
+    <div>
+      <NavBar />
+      <div>
+        <div class="max-w-screen-xl mx-auto">
+          <header>
+            {/* <h1 className="mt-24 text-2xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-3xl sm:leading-none md:text-4xl">About</h1> */}
+          </header>
+          <main className="mt-8 p-2">
+            <div
+              className="mb-4 md:mb-0 w-full max-w-screen-md mx-auto relative"
+              style={{ height: "24em" }}
+            >
+              <div
+                className="absolute left-0 bottom-0 w-full h-full z-10"
+                style={{
+                  backgroundImage:
+                    "linear-gradient`(180deg,transparent,rgba(0,0,0,.7))",
+                }}
+              ></div>
+              <img
+                src="https://images.unsplash.com/photo-1590989995665-1815e7b2c42b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1526&q=80"
+                className="absolute left-0 top-0 w-full h-full z-0 object-cover"
+              />
+              <div className="p-4 absolute bottom-0 left-0 z-20">
+                <h2 className="text-4xl font-semibold text-gray-100 leading-tight">
+                  #BlackLivesMatter
+                </h2>
+              </div>
+            </div>
 
-<NavBar />
-<div>
-<div class="max-w-screen-xl mx-auto">
-<header>
+            <div className="px-4 lg:px-0 mt-12 text-gray-700 max-w-screen-md mx-auto text-lg leading-relaxed">
+              <div className="border-l-4 border-gray-500 pl-4 mb-6 italic rounded text-left">
+                The rate at which black Americans are killed by police is more
+                than twice as high as the rate for white Americans.
+                <span className="pr-2"> ~ LA Johnson/NPR </span>
+              </div>
+              <p className="pb-6 text-left break-words">
+                {" "}
+                The racial inequality website was made not only highlight police
+                brutality and racism in America, but also a way forward to a
+                better tomorrow. To create a nation where we can all say that
+                <span className="italic text-gray-600">
+                  {" "}
+                  "We hold these truths to be self-evident, that all men are
+                  created equal, that they are endowed by their Creator with
+                  certain unalienable Rights, that among these are Life, Liberty
+                  and the pursuit of Happiness".
+                </span>
+              </p>
+            </div>
+          </main>
+          <h1 className="mt-2 text-2xl tracking-tight leading-1 font-extrabold text-gray-900 sm:text-3xl sm:leading-none md:text-4xl">
+            Know their names
+          </h1>
+          <h3 className="px-10 sm:pt-4 text-sm tracking-tight leading-1 font-normal text-gray-900 sm:text-md sm:leading-none md:text-lg">
+            A non-comprehensive list of black people that have died as a result
+            of police brutality and/or racsim. There are many, many more.
+          </h3>
+          <AboutPageList />
 
-{/* <h1 className="mt-24 text-2xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-3xl sm:leading-none md:text-4xl">About</h1> */}
-
-
-</header>
-    <main class="mt-8 p-2">
-
-      <div class="mb-4 md:mb-0 w-full max-w-screen-md mx-auto relative" style={{height: "24em"}}>
-        <div class="absolute left-0 bottom-0 w-full h-full z-10"
-          style={{backgroundImage: "linear-gradient`(180deg,transparent,rgba(0,0,0,.7))"}}></div>
-        <img src="https://images.unsplash.com/photo-1590989995665-1815e7b2c42b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1526&q=80" 
-        className="absolute left-0 top-0 w-full h-full z-0 object-cover" />
-        <div class="p-4 absolute bottom-0 left-0 z-20">
-          <h2 class="text-4xl font-semibold text-gray-100 leading-tight">
-           #BlackLivesMatter
-          </h2>
-      
         </div>
       </div>
 
-      <div class="px-4 lg:px-0 mt-12 text-gray-700 max-w-screen-md mx-auto text-lg leading-relaxed">
-
-        <div class="border-l-4 border-gray-500 pl-4 mb-6 italic rounded text-left">
-        The rate at which black Americans are killed by police is more than twice as high as the rate for white Americans.
-<span className="pr-2"> ~ LA Johnson/NPR </span>
-        </div>
-        <p class="pb-6 text-left break-words"> The racial inequality website was made not only highlight police brutality and racism in America, 
-        but also a way forward to a better tomorrow. To create a nation where we can all say that 
-
-        <span className="italic text-gray-600" > "We hold these truths to be self-evident, that all men are created equal, 
-        that they are endowed by their Creator with certain unalienable Rights, 
-        that among these are Life, Liberty and the pursuit of Happiness".</span></p>
-
+      <div className="container mx-auto mt-2 p-4 text-gray-900 text-center break-words">
+        EMMETT TILL - MEDGAR EVERS - GEORGE JUNIUS STINNEY JR. - DR MARTIN
+        LUTHER KING JR - HENRY SMITH - JOHN CRAWFORD III - MICHAEL BROWN - EZELL
+        FORD - DANTE PARKER - MICHELLE CUSSEAUX - MARY TURNER - LAQUAN MCDONALD
+        - MALCOLM X - TANISHA ANDERSON - AKAI GURLEY - TAMIR RICE - RUMAIN
+        BRISBON - JERAME REID - MATTHEW AJIBADE - JAMES N. POWELL JR. - FRANK
+        SMART - ERNEST LACY - NATASHA MCKENNA - TONY ROBINSON - ANTHONY HILL -
+        MYA HALL - PHILLIP WHITE - ERIC HARRIS - WALTER SCOTT - WILLIAM CHAPMAN
+        II - ALEXIA CHRISTIAN - BRENDON GLENN - VICTOR MANUEL LAROSA - JONATHAN
+        SANDERS - FREDDIE CARLOS GRAY JR. - JOSEPH MANN - SALVADO ELLSWOOD -
+        SANDRA BLAND - ALBERT JOSEPH DAVIS - DARRIUS STEWART - BILLY RAY DAVIS -
+        SAMUEL DUBOSE - MICHAEL SABBIE - BRIAN KEITH DAY - CHRISTIAN TAYLOR -
+        TROY ROBINSON - ASSHAMS PHAROAH MANLEY - MICHAEL STEWART - FELIX KUMI -
+        KEITH HARRISON MCLEOD - JUNIOR PROSPER - LAMONTEZ JONES - PATERSON BROWN
+        - DOMINIC HUTCHINSON - ANTHONY ASHFORD - ALONZO SMITH - TYREE CRAWFORD -
+        INDIA KAGER - LA?VANTE BIGGS - MICHAEL LEE MARSHALL - JAMAR CLARK -
+        RICHARD PERKINS - PHILLIP PANNELL - NATHANIEL HARRIS PICKETT - BENNI LEE
+        TIGNOR - MIGUEL ESPINAL - MICHAEL NOEL - KEVIN MATTHEWS - BETTIE JONES -
+        QUINTONIO LEGRIER - KEITH CHILDRESS JR. - JANET WILSON - RANDY NELSON -
+        ANTRONIE SCOTT - WENDELL CELESTINE - DAVID JOSEPH - CALIN ROQUEMORE -
+        DYZHAWN PERKINS - CHRISTOPHER DAVIS - MARCO LOUD - JAMES BYRD JR. -
+        PETER GAINES - TORREY ROBINSON - DARIUS ROBINSON - KEVIN HICKS - MARY
+        TRUXILLO - DEMARCUS SEMER - AMADOU DIALLO - WILLIE TILLMAN - TERRILL
+        THOMAS - DEMETRIUS DUBOSE - ALTON STERLING - PHILANDO CASTILE - TERENCE
+        CRUTCHER - PAUL O?NEAL - ALTERIA WOODS - BOBBY RUSS - JORDAN EDWARDS -
+        AARON BAILEY - RONELL FOSTER - STEPHON CLARK - COREY CARTER - ANTWON
+        ROSE II - TAYLER ROCK - MALICE GREEN - RAMARLEY GRAHAM - ELIJAH MCCLAIN
+        - AIYANA STANLEY JONES - BOTHAM JEAN - PAMELA TURNER - DOMINIQUE CLAYTON
+        - SEAN BELL - ATATIANA JEFFERSON - JEMEL ROBERSON - JAMES LEE ALEXANDER
+        - RYAN MATTHEW SMITH - DERRICK AMBROSE JR. - ADDIE MAE COLLINS - CAROL
+        DENISE MCNAIR - CAROLE ROBERTSON - CYNTHIA WESLEY - NICHOLAS HEYWARD JR.
+        - CHRISTOPHER WHITFIELD - WILLIE MCCOY - VICTOR WHITE III - MARCUS DEON
+        SMITH - CHAVIS CARTER - MARTIN LEE ANDERSON - CHRISTOPHER MCCORVEY -
+        BRADLEY BLACKSHIRE - TIMOTHY THOMAS - REGINALD DOUCET JR. - DANROY "DJ"
+        HENRY JR. - KARVAS GAMBLE JR. - ERIC REASON - KORRYN GAINES - REKIA BOYD
+        - KIONTE SPENCER - DARIUS TARVER - WAYNE ARNOLD JONES - MANUEL ELLIS -
+        VICTOR DUFFY JR. - KOBE DIMOCK-HEISLER - CLINTON R. ALLEN - DONTRE
+        HAMILTON - TIMOTHY CAUGHMAN - SYLVILLE SMITH - COREY JONES - TYRE KING -
+        ERIC GARNER - MILES HALL - KENDRICK JOHNSON - CHARLEENA LYLES - MICHAEL
+        LORENZO DEAN - TRAYVON MARTIN - RENISHA MCBRIDE - KIWANE CARRINGTON -
+        OSCAR GRANT III - BREONNA TAYLOR - KALIEF BROWDER - DARRIEN HUNT - TROY
+        HODGE - WILLIAM GREEN - AHMAUD ARBERY - DION JOHNSON - TONY MCDADE -
+        JAMEL FLOYD - GEORGE FLOYD - RAYSHARD BROOKS - ITALIA MARIE KELLY -
+        DAVID MCATEE - CHRIS BEATY
       </div>
-    </main>
-    <h1 className="mt-2 text-2xl tracking-tight leading-1 font-extrabold text-gray-900 sm:text-3xl sm:leading-none md:text-4xl">Know their names</h1>
-    <h3 className="px-10 text-left text-sm tracking-tight leading-1 font-normal text-gray-900 sm:text-md sm:leading-none md:text-lg">A non-comprehensive list of black people that have died as a result of police brutality and/or racsim. There are many, many more.</h3>
-    {/* <AboutPageList /> */}
-
-  </div>
-
-</div>
-
-
-<div className="container mx-auto mt-2 p-4 text-gray-900 text-center break-words">
-
-EMMETT TILL - MEDGAR EVERS - GEORGE JUNIUS STINNEY JR. - DR MARTIN LUTHER KING JR - HENRY SMITH - JOHN CRAWFORD III - MICHAEL BROWN - EZELL FORD - DANTE PARKER - MICHELLE CUSSEAUX - MARY TURNER - LAQUAN MCDONALD - MALCOLM X - TANISHA ANDERSON - AKAI GURLEY - TAMIR RICE - RUMAIN BRISBON - JERAME REID - MATTHEW AJIBADE - JAMES N. POWELL JR. - FRANK SMART - ERNEST LACY - NATASHA MCKENNA - TONY ROBINSON - ANTHONY HILL - MYA HALL - PHILLIP WHITE - ERIC HARRIS - WALTER SCOTT - WILLIAM CHAPMAN II - ALEXIA CHRISTIAN - BRENDON GLENN - VICTOR MANUEL LAROSA - JONATHAN SANDERS - FREDDIE CARLOS GRAY JR. - JOSEPH MANN - SALVADO ELLSWOOD - SANDRA BLAND - ALBERT JOSEPH DAVIS - DARRIUS STEWART - BILLY RAY DAVIS - SAMUEL DUBOSE - MICHAEL SABBIE - BRIAN KEITH DAY - CHRISTIAN TAYLOR - TROY ROBINSON - ASSHAMS PHAROAH MANLEY - MICHAEL STEWART - FELIX KUMI - KEITH HARRISON MCLEOD - JUNIOR PROSPER - LAMONTEZ JONES - PATERSON BROWN - DOMINIC HUTCHINSON - ANTHONY ASHFORD - ALONZO SMITH - TYREE CRAWFORD - INDIA KAGER - LA?VANTE BIGGS - MICHAEL LEE MARSHALL - JAMAR CLARK - RICHARD PERKINS - PHILLIP PANNELL - NATHANIEL HARRIS PICKETT - BENNI LEE TIGNOR - MIGUEL ESPINAL - MICHAEL NOEL - KEVIN MATTHEWS - BETTIE JONES - QUINTONIO LEGRIER - KEITH CHILDRESS JR. - JANET WILSON - RANDY NELSON - ANTRONIE SCOTT - WENDELL CELESTINE - DAVID JOSEPH - CALIN ROQUEMORE - DYZHAWN PERKINS - CHRISTOPHER DAVIS - MARCO LOUD - JAMES BYRD JR. - PETER GAINES - TORREY ROBINSON - DARIUS ROBINSON - KEVIN HICKS - MARY TRUXILLO - DEMARCUS SEMER - AMADOU DIALLO - WILLIE TILLMAN - TERRILL THOMAS - DEMETRIUS DUBOSE - ALTON STERLING - PHILANDO CASTILE - TERENCE CRUTCHER - PAUL O?NEAL - ALTERIA WOODS - BOBBY RUSS - JORDAN EDWARDS - AARON BAILEY - RONELL FOSTER - STEPHON CLARK - COREY CARTER - ANTWON ROSE II - TAYLER ROCK - MALICE GREEN - RAMARLEY GRAHAM - ELIJAH MCCLAIN - AIYANA STANLEY JONES - BOTHAM JEAN - PAMELA TURNER - DOMINIQUE CLAYTON - SEAN BELL - ATATIANA JEFFERSON - JEMEL ROBERSON - JAMES LEE ALEXANDER - RYAN MATTHEW SMITH - DERRICK AMBROSE JR. - ADDIE MAE COLLINS - CAROL DENISE MCNAIR - CAROLE ROBERTSON - CYNTHIA WESLEY - NICHOLAS HEYWARD JR. - CHRISTOPHER WHITFIELD - WILLIE MCCOY - VICTOR WHITE III - MARCUS DEON SMITH - CHAVIS CARTER - MARTIN LEE ANDERSON - CHRISTOPHER MCCORVEY - BRADLEY BLACKSHIRE - TIMOTHY THOMAS - REGINALD DOUCET JR. - DANROY "DJ" HENRY JR. - KARVAS GAMBLE JR. - ERIC REASON - KORRYN GAINES - REKIA BOYD - KIONTE SPENCER - DARIUS TARVER - WAYNE ARNOLD JONES - MANUEL ELLIS - VICTOR DUFFY JR. - KOBE DIMOCK-HEISLER - CLINTON R. ALLEN - DONTRE HAMILTON - TIMOTHY CAUGHMAN - SYLVILLE SMITH - COREY JONES - TYRE KING - ERIC GARNER - MILES HALL - KENDRICK JOHNSON - CHARLEENA LYLES - MICHAEL LORENZO DEAN - TRAYVON MARTIN - RENISHA MCBRIDE - KIWANE CARRINGTON - OSCAR GRANT III - BREONNA TAYLOR - KALIEF BROWDER - DARRIEN HUNT - TROY HODGE - WILLIAM GREEN - AHMAUD ARBERY - DION JOHNSON - TONY MCDADE - JAMEL FLOYD - GEORGE FLOYD - RAYSHARD BROOKS - ITALIA MARIE KELLY - DAVID MCATEE - CHRIS BEATY
-
-</div>
-{/* <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 mx-auto container ">
+      {/* <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 mx-auto container ">
 
 
 
 </div> */}
 
-
-
-
-<Footer />
-
-            
-        </div>
-    )
+      <Footer />
+    </div>
+  )
 }
 
 export default about
