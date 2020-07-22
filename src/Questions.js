bea5ca7a3ccdd52c2fe7ae582ff17f9fa52474bb
Kenny <kawhyte@gmail.com>
2020-07-22 16:37:24 -0700

    questions section info

--- src/Questions.js
+++ src/Questions.js
@@ -2,10 +2,10 @@ import React from "react"
 
 function Questions() {
   return (
-      <div class= "">
+      <div className= "">
         <div className=" mx-auto container flex justify-center flex-col ">
           <div className="pl-6 pr-6 pt-6">
-            <h1 className=" font-medium text-left text-xl text-gray-800 mb-5 mt-4">
+            <h1 className="font-medium text-left text-2xl text-gray-800 mb-5 mt-4">
               Frequently Asked Questions
             </h1>
           </div>
@@ -22,20 +22,27 @@ function Questions() {
                 image: `Know their names`,
               },
               {
-                question:`how can i support black lives matter? `,
-                answer: `internal`,
+                question:`how can i support black lives matter?`,
+                answer: `BLM’s #WhatMatters2020 will focus on issues concerning racial injustice, police brutality, criminal justice reform, Black immigration, economic injustice, LGBTQIA+ and human rights, environmental injustice, access to healthcare, access to quality education, and voting rights and suppression.`,
+                source:`https://blacklivesmatter.com/what-matters-2020/`,
+                sourceName:`blacklivesmatter.com`,
                 link: `/know-their-names`,
                 image: `Know their names`,
               },
+              {
+                question:`What is Racial Trauma?`,
+                answer: `Racial trauma, a form of race-based stress, refers to People of Color and Indigenous individuals’ (POCI) reactions to dangerous events and real or perceived experiences of racial discrimination. Such experiences may include threats of harm and injury, humiliating and shaming events, and witnessing racial discrimination toward other POCI.`,
+                source:`https://psycnet.apa.org/fulltext/2019-01033-001.html`,
+                sourceName:`American Psychological Association`,
+                link: `/know-their-names`,
+                image: `Know their names`,
+              },
+           
               {
                 question:`How To Be An Ally - What can I do to help?  `,
-                answer: `internal`,
-                link: `/know-their-names`,
-                image: `Know their names`,
-              },
-              {
-                question:`How do I Cope with Racial Trauma?`,
-                answer: `internal`,
+                answer: `LEAP: A Framework for Becoming a Better Ally to Black Employees. L: Listen and learn from your Black colleagues’ experiences. E: Engage with Black colleagues in racially diverse and more casual settings. A: Ask Black employees about their work and their goals. P: Provide your Black colleagues with opportunities, suggestions, encouragement, and general support. `,
+                source:`https://hbr.org/2020/07/how-to-be-a-better-ally-to-your-black-colleagues`,
+                sourceName:`Harvard Business Review`,
                 link: `/know-their-names`,
                 image: `Know their names`,
               },
@@ -49,19 +56,17 @@ function Questions() {
             ].map(card =>
               (
                 <section
-              class="p-5 flex flex-col justify-between
+              className="p-5 flex flex-col justify-between
 								bg-gray-100 dark:bg-gray-200 rounded-lg"
               href="/"
             >
               <div
-                class="flex items-center justify-between font-semibold capitalize text-left dark:text-gray-700"
+                className="flex items-center justify-between dark:text-gray-700  uppercase truncate-custom text-sm md:text-md font-semibold break-words text-gray-700 text-center"
               >
                 <span>{card.question}</span>
               </div>
 
-              <p
-                class="text-sm font-medium leading-snug text-left
-									text-gray-600 my-3"
+              <p className="font-normal leading-snug text-left  my-3  text-sm md:text-md text-gray-700 "
               >
                {card.answer}
               </p>
@@ -71,7 +76,7 @@ function Questions() {
              rel="noopener noreferrer"
              
              
-             ><p class="text-left text-sm mb-3 font-medium leading-snug text-gray-600 "> Source:<span class="ml-2">{card.sourceName} </span></p> </a> 
+             ><p className="text-left text-sm mb-1 font-light leading-snug text-gray-600 "> Source | More Info:<span className=" text-blue-600 ml-2">{card.sourceName} </span></p> </a> 
              {/* <a
              href={card.source}
              target="_blank"
